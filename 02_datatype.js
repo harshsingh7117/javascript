d730cac03e9ae12f6660f9eb2eba6ee0b7a058f7
harshsingh7117 <harshsingh7117@gmail.com>
2024-03-30 23:50:17 +0530

    chnaged a file

--- 02_datatype.js
+++ 02_datatype.js
@@ -6,7 +6,7 @@ console.log(3 + 3);// code readability should be high
 
 
 console.log("harsh");
- this are datatype.
+//  this are datatype.
 // let name = "harsh"// string datatype is written in '' '' and ' '.
 // let age = 18
 // let isLoggedIn = false
