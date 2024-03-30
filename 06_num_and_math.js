76d3e5e982ecd8c2b3672a3d4404c18a8aaccd7b
harshsingh7117 <harshsingh7117@gmail.com>
2024-03-31 02:05:58 +0530

    .

--- 06_num_and_math.js
+++ 06_num_and_math.js
@@ -1,7 +1,41 @@
 const score = 500
-console.log(score);
+//console.log(score);
 
 const balance = new Number(200)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber=123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundrerds = 1000000
+// console.log(hundrerds.toLocaleString());
+
+// ++++++++++++++++ maths +++++++++++++++++++
+
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,5,9));
+// console.log(Math.max(4,5,9));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1); it give value greate than 1 (1.2414)
+
+const min =10
+const max=20
+
+// Math.random give value between 0-1
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)
+) + min );
