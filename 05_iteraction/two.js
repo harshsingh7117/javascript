7f2327a69b2719493ed7b9625f255cd6e0ce27f2
harshsingh7117 <harshsingh7117@gmail.com>
2024-04-01 05:03:41 +0530

    .

---
+++ 05_iteraction/two.js
@@ -0,0 +1,24 @@
+//  while loop
+
+let index=0
+while (index <= 10) {
+    
+    // console.log(`value of index is ${index}`);
+    index = index +2
+}
+
+let myarr=["flash","batman","superman"]
+
+let arr =0
+while (arr<myarr.length) {
+    // console.log(`valu is ${myarr[arr]}`);
+    arr =arr +1
+    
+}
+
+let score = 15
+
+do{
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
