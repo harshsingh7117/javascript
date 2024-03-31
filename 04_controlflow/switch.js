a6d141149118133edcf59aee1445876b7e27a2a8
harshsingh7117 <harshsingh7117@gmail.com>
2024-04-01 04:14:37 +0530

    .

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("fab");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+        default:
+            console.log("default value");
+            break;
+}
---
+++ 04_controlflow/values.js
@@ -0,0 +1,38 @@
+const useremail = "h@gmail.com"
+
+if(useremail){
+    console.log("got email")
+} else {
+    console.log("don't have email")
+}
+
+// falsy values
+
+// false,0,-0,0n,BigInt,"",null,undefined,NaN
+
+//truthy values 
+
+// 1,"0","false"," ",[],{},function(){}
+
+// if (useremail.length === 0){
+//     console.log("arr empty");
+// }
+
+const emptyobj ={}
+
+// if(Object.keys(emptyobj).length === 0){
+//     console.log("empty");
+// }
+
+// false == 0 (true)
+// false == "" (true)
+// 0 == ""(true)
+
+// nullish Coalescing Operator (??) : null undefined (it is used for sefty of null value.)
+
+let val1;
+// val1 = 5??10
+// val1 = null ?? 10
+val1 = undefined ?? 15
+
+console.log(val1);
