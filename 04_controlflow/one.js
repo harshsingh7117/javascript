961ef56a576d18adf7c61103687db76b67496cbe
harshsingh7117 <harshsingh7117@gmail.com>
2024-04-01 03:49:31 +0530

    .

---
+++ 04_controlflow/one.js
@@ -0,0 +1,53 @@
+// if
+const isuserloggedIn = true
+const temperature= 41
+
+// if(temperature === 41){
+//    console.log("less then 50")
+// }
+//    else{
+//        console.log("temperature is greater than 50");
+//     }
+// console.log("exicute");
+
+// Compressionoperater: <,>,<=,>=,==(equal),!=(notequal),=(asigment operater),===(check type),!==(negative signe)
+
+
+// const score = 200
+
+// if(score >100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`)
+// }
+
+// console.log(`user power: ${power}`)
+
+const balance = 1000
+
+//if (balance> 500) console.log("test"); // emplicit scope it is exicute in one line.
+
+// if (balance < 500){
+//      console.log("less than ");
+// } else if(balance < 750){
+//     console.log("less than 500");
+// } else if(balance <900){
+//     console.log("less than 750");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const LoggedInGoogle = false
+const LoggedInEmail = true
+
+// if (userLoggedIn && debitcard && 2 == 2){
+//     console.log("allow to buy cources");
+
+// }
+
+if (LoggedInEmail || LoggedInGoogle){
+    console.log("user Loggged In");
+
+}
+
